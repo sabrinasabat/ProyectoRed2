f5dbe09dadcd72e6b5401da74cef60bf92866151
sabrinasabat <sabrina.s354970@cesurformacion.com>
2024-03-21 08:53:38 +0100

    21.03.24

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,19 @@
 package main;
-
+import dominio.Aparato;
+import dominio.Red;
 public class Main {
+    public static void main(String[] args) {
+        Aparato tele = new Aparato(30);
+
+        tele.encender();
+
+        Red miRed = new Red(25);
+        miRed.addAparato(tele);
+
+        System.out.println("tele encendida? "+ tele.getEncendido()+ ", consumo: " + tele.getConsumo());
+        System.out.println("mi red tiene "+miRed.size()+ " aparatos");
+        System.out.println("consumo actual red "+ miRed.getConsumoActual());
+        System.out.println("es la red estable? "+ (miRed.esEstable()));
+    }
+
 }
